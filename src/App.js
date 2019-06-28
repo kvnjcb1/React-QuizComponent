b9ab5f1fe486608ba4063e666b5ff54fef1c9160
Kevin Jacob <kevin96jacob@gmail.com>
2019-06-28 16:16:35 +0530

    add existing file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import { Quiz } from "./Quiz"
+import  Quiz  from "./Quiz"
 import './App.css'
 
 class App extends Component {
   render() {
     return (
-     <Quiz></Quiz>
+      <Quiz />
     )
   }
 }
